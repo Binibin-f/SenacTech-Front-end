30d2a1ef2659fb8f300af352cd06e761b50efe06
Bin <binifidelis@gmail.com>
2024-11-25 11:35:30 -0300

    Anotações e exemplos

---
+++ Anotações/AtiviTeste/script.js
@@ -0,0 +1,67 @@
+//titulo.innerText="Mudou viu"
+//as vezes é mais facil
+
+//burro faz trabalho dobrado
+/*function ale(aleatorio){
+
+    let aleatorio = (imgRobo.src= "htt", "https:/87", "https://roborg/987")
+
+    ale(aleatorio[0])
+    ale(aleatorio[1])
+    ale(aleatorio[2])
+
+}
+
+btTrocarRobo.addEventListener("click", trocarDeRobo)
+
+function trocarDeRobo(){
+    imgRobo.src="https://robohash.org/"+Math.random()
+}
+
+
+//imgRobo.src="https://robohash.org/"+Math.random()
+
+function ale(){
+    
+    let aleatorios = ["https://robohash.org/991", "https://robohash.org/5221", "https://robohash.org/521"]
+
+    imgRobo.src = aleatorios[2]
+}
+
+ale()
+
+function ale(aleatorio){
+    imgRobo.src = aleatorio
+    
+}
+    let aleatorio = ["https://robohash.org/992", "https://robohash.org/5221", "https://robohash.org/561"]
+
+    ale(aleatorio[0])
+    ale(aleatorio[1])
+    ale(aleatorio[2])
+
+
+
+    let robo = parseInt(Math.random(0)+1)
+
+
+
+    function ale(aleatorio){
+        imgRobo.src = aleatorio
+        
+    }
+        let aleatorio = ["https://robohash.org/992", "https://robohash.org/5221", "https://robohash.org/561"]*/
+    
+
+
+        let url = [
+            "https://robohash.org/992", 
+            "https://robohash.org/5221", 
+            "https://robohash.org/561"
+        ]
+
+        let num;
+
+        num = parseInt (Math.random()* url.length)
+        imgRobo.src=url[num]
+    
